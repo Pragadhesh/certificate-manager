f1be003d6d9aff7b2b5508f5a5aca0000ef409cc
PragadheshG <pragadhesh.gopalakrishnan@gds.ey.com>
2022-09-21 11:59:56 +0530

    Added Initial UI

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ForgeUI, {
     Link,
     Macro,
     Text,
-    TextArea,
+    DatePicker,
     TextField,
     render,
     useConfig,
@@ -14,166 +14,63 @@ import ForgeUI, {
     Strong,
   } from "@forge/ui";
   import api, { route } from "@forge/api";
-  
+
   const STATE = {
-    INITIAL: 0,
+    TITLE: 0,
     INPUT: 1,
     SUCCESS: 2,
   };
   
-  type CreateIssueResponse = {
-    key?: string;
-    errorMessages: string[];
-    errors?: Map<string, string>;
-  };
-  
   const App = () => {
-    const { accountId } = useProductContext();
-    const config = useConfig();
-    const [state, setState] = useState(STATE.INITIAL);
-    const [error, setError] = useState(null);
-    const [issueKey, setIssueKey] = useState(null);
-  
-    if (!config) {
-      return doNeedConfig();
-    }
-  
+    const [state, setState] = useState(STATE.TITLE)
+    
     switch (state) {
-      case STATE.INITIAL:
-        return doInitial();
-      case STATE.INPUT:
-        return doInput();
-      case STATE.SUCCESS:
-        return doSuccess();
-    }
-  
-    function doNeedConfig() {
-      return <Text>This app requires configuration before use.</Text>;
-    }
-  
-    function doInitial() {
-      return (
-        <Fragment>
-          <Button
-            text="📣 Leave feedback on this page"
-            onClick={() => {
-              setState(STATE.INPUT);
-            }}
-          />
-        </Fragment>
-      );
-    }
-  
-    function doInput() {
-      return (
-        <Fragment>
-          <Form onSubmit={createIssue}>
-            {error ? (
-              <Text>
-                <Strong> {`Error: ${error}`}</Strong>
-              </Text>
-            ) : (
-              <Text>
-                <Strong>Please complete the fields below:</Strong>
-              </Text>
-            )}
-            <TextField label="Title" name="summary" isRequired={true} />
-            <TextArea label="Description" name="description" />
-          </Form>
-        </Fragment>
-      );
-    }
-  
-    function doSuccess() {
-      return (
-        <Fragment>
-          <Text>
-            🙏{" "}
-            <Strong>
-              Created <Link href={`/browse/${issueKey}`}>{issueKey}</Link> Thanks
-              for your feedback!
-            </Strong>
-          </Text>
-          <Button
-            text="📣 Leave more feedback"
-            onClick={() => {
-              setState(STATE.INPUT);
-            }}
-          />
-        </Fragment>
-      );
-    }
-  
-    async function createIssue({ summary, description }) {
-      setIssueKey(null);
-      const payload: any = {
-        update: {},
-        fields: {
-          project: {
-            key: config.projectKey,
-          },
-          issuetype: {
-            id: config.issueTypeId,
-          },
-          assignee: {
-            id: config.assignee,
-          },
-          summary,
-          description: {
-            type: "doc",
-            version: 1,
-            content: [
-              {
-                type: "paragraph",
-                content: [
-                  {
-                    text: description,
-                    type: "text",
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      };
-      // Only add the report if necessary since Jira's default setup omits the reporter field
-      // from screens and will result in an error.
-      if (config.reporter === "author") {
-        payload.fields.reporter = {
-          id: accountId,
-        };
-      }
-      const response = await api.asApp().requestJira(route`/rest/api/3/issue`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      });
-  
-      const responseBody: CreateIssueResponse = await response.json();
-      if (!response.ok) {
-        console.error(responseBody);
-        const firstErrorMessage = responseBody.errorMessages[0];
-        let errorMessage = firstErrorMessage ? firstErrorMessage : "";
-        if (responseBody.errors) {
-          const messageSeparator = ". ";
-          const additionalErrorText = Object.values(responseBody.errors).join(
-            messageSeparator
-          );
-          if (additionalErrorText) {
-            errorMessage = errorMessage
-              ? errorMessage + messageSeparator + additionalErrorText
-              : additionalErrorText;
-          }
+        case STATE.TITLE:
+            return doTitle();
+        case STATE.INPUT:
+            return doInput();
         }
-        setError(errorMessage || "Failed to create issue.");
-      } else {
-        setError(null);
-        setIssueKey(responseBody.key);
-        setState(STATE.SUCCESS);
+
+    function doTitle() {
+        return (
+          <Fragment>
+            <Button
+              text="📣 Add Certificates"
+              onClick={() => {
+                setState(STATE.INPUT);
+              }}
+            />
+          </Fragment>
+        );
       }
+
+      async function onSubmit(formData) {
+        /**
+         * formData:
+         * {
+         *    username: 'Username',
+         *    products: ['jira']
+         * }
+         */
+        console.log(formData)
+      };
+
+    function doInput() {
+        return (
+            <Fragment>
+                <Form onSubmit={onSubmit}>
+                <TextField label="Certificate Name" name="certificate-name" isRequired={true} />
+                <DatePicker label="Issued On" name="issued-on" isRequired={true}/>
+                <DatePicker label="Expires On" name="expires-on" isRequired={true}/>
+                </Form>
+            </Fragment>
+                );
+    }
+
+
+
+    async function createCertificate(params:string) {
+        console.log("Reached function")
     }
   };
   
